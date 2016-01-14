6b33557aa73bfca149d205357413eacab201b2c6
codecly259 <maxinchun0215@qq.com>
2016-01-14 15:33:02 +0800

    blog首页展示文章列表

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,8 +9,10 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		Post frontPost = Post.find("order by postedAt desc").first();
+		List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+		render(frontPost, olderPosts);
+	}
 
 }
