2a3268d0f9604a5fa9d5e9a8eff30af53c034fbd
codecly259 <maxinchun0215@qq.com>
2016-01-15 10:47:13 +0800

    添加评论和验证

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -24,6 +25,15 @@ public class Application extends Controller {
 	public static void show(Long id) {
 		Post post = Post.findById(id);
 		render(post);
+	}
+	
+	public static void postComment(Long postId, @Required String author, @Required String content) {
+		Post post = Post.findById(postId);
+		if(validation.hasErrors()){
+			render("Application/show.html", post);
+		}
+		post.addComment(author, content);
+		show(postId);
 	}
 
 }
